581c6802dd9a3f2550b9374e7a9ff6851b67c2c6
Geroen Joris <geroen.joris@kb.vlaanderen.be>
2019-01-17 21:14:15 +0100

    chore() Initial commit

---
+++ src/main/java/eu/droidit/solid/javasolidserver/JavaSolidServerApplication.java
@@ -0,0 +1,14 @@
+package eu.droidit.solid.javasolidserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSolidServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSolidServerApplication.class, args);
+    }
+
+}
+
